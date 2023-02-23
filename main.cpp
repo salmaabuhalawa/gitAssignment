dbcbefaaf2ba496953ffa85d2dd401d36ac3b4a2
salmaabuhalawa <89554552+salmaabuhalawa@users.noreply.github.com>
2023-02-23 18:00:29 +0200

    Add Minimum function in minimum branch

--- main.cpp
+++ main.cpp
@@ -1,15 +1,21 @@
 #include<iostream>
 #include<vector>
 #include<numeric>
+#include <bits/stdc++.h>
 using namespace std;
 
 int getSum(vector<int> numbers){
     return accumulate(numbers.begin(), numbers.end(), 0);
 }
 
+int getMin(vector<int> numbers){
+    return *min_element(numbers.begin(), numbers.end());
+}
+
 int main() {
    vector<int> numbers = {1,2,3,4,5,6,7,8,9,10};
    cout<<"Sum of numbers is: "<< getSum(numbers)<<"\n";
+   cout<<"Minimum Element is: "<< getMin(numbers)<<"\n";
 
    return 0;
 }
